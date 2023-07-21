b9706a999628930c8ac09737c044c98a0e6660d6
meitipro <103358103+meitipro@users.noreply.github.com>
2023-07-21 16:22:16 +0330

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x5ab5d88e5ff10f1e3eade88ca94f10960a890069";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
